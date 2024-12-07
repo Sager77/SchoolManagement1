efaad749daee5c41e16023263bc8fd0b84236268
Sage77 <ssager348@gmail.com>
2024-12-07 08:09:42 +0500

    env file update

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
  async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/persondb');
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
--- backend/models/connection.js
+++ backend/models/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/persondb');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
