11b98a9bb6c4a6eb01f126debe008b0315f61212
Sage77 <ssager348@gmail.com>
2024-12-20 22:56:25 +0500

    ass

--- frontend/src/components/UserCard.js
+++ frontend/src/components/UserCard.js
@@ -97,7 +97,7 @@ const UserCard = () => {
             </div>
             <div className="col-md-4 d-flex justify-content-center mt-4">
               <img
-                src={`http://localhost:5000/uploads/${userData.userimage}`}
+                src={`http://dashboard.validate.com/uploads/${userData.userimage}`}
                 alt="User"
                 className="img-fluid"
                 style={{ maxWidth: '150px' }}
